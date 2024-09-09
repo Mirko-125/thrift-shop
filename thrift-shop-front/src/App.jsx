48b61cdcbad3e07f061dfb1938403715ded54112
Mirko <mirkoat21@gmail.com>
2024-09-10 01:26:14 +0200

    Still learning, it will take some time

--- thrift-shop-front/src/App.jsx
+++ thrift-shop-front/src/App.jsx
@@ -1,6 +1,18 @@
 import { Button } from "@/components/ui/button"
 import './App.css'
 
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu"
+
+
 import { useNavigate } from 'react-router-dom';
 
 function App() 
@@ -8,7 +20,21 @@ function App()
     const navigate = useNavigate();
     return (
       <>
-        <Button onClick={() => navigate('/NewPage')}>Click me!</Button>
+        <div className="navBar">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <NavigationMenuLink>Link</NavigationMenuLink>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            </NavigationMenuList>  
+          </NavigationMenu>
+        </div>
+        <div className="frontPage">
+            <Button onClick={() => navigate('/NewPage')}>Click me!</Button>
+        </div>
       </>
     )
 }
