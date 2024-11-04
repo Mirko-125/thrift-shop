dba3968f5e37df20652446f3e002baa95a6aa58e
Mirko <mirkoat21@gmail.com>
2024-11-04 20:25:30 +0100

    Testing

--- thrift-shop-front/src/links.js
+++ thrift-shop-front/src/links.js
@@ -1,3 +1,5 @@
 export const background = "http://localhost:8888/background_inspo.jpg"
 export const articles = "http://localhost:8888/articles.json" // not using it for now
 export const url = "http://localhost:8888"
+
+// don't forget to update all links
