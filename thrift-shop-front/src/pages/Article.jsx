3dd6404e40a2658c48c332f60ab7ded909d3368b
Mirko <mirkoat21@gmail.com>
2024-11-08 23:04:39 +0100

    Working on some animation and connections over the shopping page

--- thrift-shop-front/src/pages/Article.jsx
+++ thrift-shop-front/src/pages/Article.jsx
@@ -1,14 +1,16 @@
+import React from "react";
+
 import Navbar from "@/components/my-components/Navbar";
 import Scroll from "@/components/my-components/Scroll";
 import Footer from "@/components/my-components/Footer";
 
 import logoSvg from '/punk.png';
+import favoriteSvg from '/favorite.svg';
 
 import { url } from '../links.js';
 
 function Article() 
 {
-    
     const tempArticle = 
     {
         "idcode": "BLS0148",
@@ -47,9 +49,35 @@ function Article()
                 </div>
                 <div className="a-shopping">
                     <img className="punk-logo-title-bigger" src={logoSvg} alt="LOGO" />
-                    <div className="article-name">{tempArticle.name}</div>
-                    <div className="article-idcode">{tempArticle.idcode}</div>
-                    <div className="article-price">${tempArticle.price}</div>
+                    <div className="subtitle">{tempArticle.name}</div>
+                    <div className="code">{tempArticle.idcode}</div>
+                    <div className="price">${tempArticle.price}</div>
+                    <br />
+                    <div className="colour">{tempArticle.colour}</div>
+                    <div> color logo soon </div>
+                    <br />
+                    <div className="underline"/>
+                    <div className="sizes-t">Sizes</div>
+                        <div className="sizes">
+                        {tempArticle.size.map((size, index) => (
+                            <div key={index} className="size">
+                                {size}
+                            </div>
+                        ))}
+                        </div>
+                    <br />
+                    <br />
+                    <div className="buy-or-favorite">
+                        <button className="form-submit">
+                            Add to shopping bag
+                            <span>Good taste!</span>
+                        </button>
+                        <button onClick={()=>console.log("PRINTF")}>
+                            <div className="tooltip" data-tooltip="Add to favorites">
+                                <img className="diamond" src={favoriteSvg} alt="love"/>
+                            </div>
+                        </button>
+                    </div>
                 </div>
             </div>
             <br />
