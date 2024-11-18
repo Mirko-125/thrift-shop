ccc48abc4dc7d7c156e95654cd4551db74cb973d
Mirko <mirkoat21@gmail.com>
2024-11-18 23:55:43 +0100

    Added validation checks, still needs server connection tho and forgot password logic too!

--- thrift-shop-front/src/components/my-components/_SignInFill.jsx
+++ thrift-shop-front/src/components/my-components/_SignInFill.jsx
@@ -1,4 +1,5 @@
 import React, { useState, Suspense, lazy } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Registration = lazy(() => import('./__Registration.jsx'));
 const ForgotPassword = lazy(() => import('./__ForgotPassword.jsx'));
@@ -11,6 +12,11 @@ const ComponentMap =
 
 const SignInFill = () =>
 {
+    const [username, setUsername] = useState('');
+    const [passphrase, setPassphrase] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+
     const [isChosen, setChoice] = useState(null);
 
     const ComponentToRender = ComponentMap[isChosen] || null;
@@ -25,6 +31,35 @@ const SignInFill = () =>
     {
         setChoice(null);
     };
+
+    const validateForm = async (event) => {
+        event.preventDefault();
+
+        // Mock API call to validate user credentials
+        // const isValidUser = await mockApiCall(username, passphrase);
+        const isValidUser = false;
+        console.log(username, passphrase);
+
+        if (!username || !passphrase) {
+            setErrorMessage('Username and password are required!');
+            setTimeout(() => {
+                setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        if (isValidUser) {
+            setErrorMessage('');
+            navigate('/new-arrivals');
+        } else {
+            setErrorMessage('Invalid username or password!');
+            setTimeout(() => {
+                setErrorMessage('');
+            }, 4000);
+            return;
+        }
+    };
+
 
     return (
         <>
@@ -51,16 +86,17 @@ const SignInFill = () =>
                             <div className="fill-in-area">
                                 <div className="form">
                                     <label className="flabel" for="email-or-phone">E-mail or phone number</label>
-                                    <input className="finput" type="text" id="email-or-phone" name="email-or-phone" />
+                                    <input className="finput" type="text" id="email-or-phone" name="email-or-phone" onChange={(e) => setUsername(e.target.value)} />
                                     <span className="input-border"></span>
                                 </div>
                                 <div className="form">
                                     <label className="flabel" for="passphrase">Password</label>
-                                    <input className="finput" type="password" id="passphrase" name="passphrase" />
+                                    <input className="finput" type="password" id="passphrase" name="passphrase" onChange={(e) => setPassphrase(e.target.value)} />
                                     <span className="input-border"></span>
                                 </div>
                                 <br />
-                                <button className="form-submit"><span class="text">Sign in</span><span>Welcome</span></button>
+                                <button onClick={validateForm}className="form-submit"><span class="text">Sign in</span><span>Welcome</span></button>
+                                <p className="error">{errorMessage}</p>
                             </div>
                             <div className="infopult">
                                 <a className="sign-in-options" onClick={()=>handleChoice("Registration")}>
--- thrift-shop-front/src/components/my-components/__Registration.jsx
+++ thrift-shop-front/src/components/my-components/__Registration.jsx
@@ -1,42 +1,121 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Registration = () =>
-{
+const Registration = () => {
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = React.useState('');
+    const [formData, setFormData] = React.useState({
+        name: '',
+        email: '',
+        phone: '',
+        passphrase: '',
+        c_passphrase: ''
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const validateForm = async (event) => {
+        event.preventDefault();
+
+        if (!formData.name) {
+            setErrorMessage('Name is required!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!formData.email) {
+            setErrorMessage('E-mail is required!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        } else if (!emailPattern.test(formData.email)) {
+            setErrorMessage('E-mail is not valid!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        const phonePattern = /^\d{10}$/;
+        if (!formData.phone) {
+            setErrorMessage('Phone number is required!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        } else if (!phonePattern.test(formData.phone)) {
+            setErrorMessage('Phone number is not valid!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        if (!formData.passphrase) {
+            setErrorMessage('Password is required!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        if (formData.passphrase !== formData.c_passphrase) {
+            setErrorMessage('Passwords do not match!');
+            setTimeout(() => {
+            setErrorMessage('');
+            }, 4000);
+            return;
+        }
+
+        navigate('/new-arrivals');
+    };
+
     return (
         <>
             <div className="subtitle">Registration</div>
-                <div className="fill-in-area">
-                    <div className="form">
-                        <label className="flabel" for="name">Full name</label>
-                        <input className="finput" type="text" id="name" name="name" />
-                        <span className="input-border"></span>
-                    </div>
-                    <div className="form">
-                        <label className="flabel" for="email">E-mail</label>
-                        <input className="finput" type="text" id="email" name="email" />
-                        <span className="input-border"></span>
-                    </div>
-                    <div className="form">
-                        <label className="flabel" for="phone">Phone number</label>
-                        <input className="finput" type="text" id="phone" name="phone" />
-                        <span className="input-border"></span>
-                    </div>
-                    <div className="form">
-                        <label className="flabel" for="passphrase">Password</label>
-                        <input className="finput" type="password" id="passphrase" name="passphrase" />
-                        <span className="input-border"></span>
-                    </div>
-                    <div className="form">
-                        <label className="flabel" for="c_passphrase">Confirm password</label>
-                        <input className="finput" type="password" id="c_passphrase" name="c_passphrase" />
-                        <span className="input-border"></span>
-                    </div>
-                    <br />
-                    <button className="form-submit"><span class="text">Register</span><span>Hello</span></button>
-                    <br />
-                    <br />
+            <div className="fill-in-area">
+                <div className="form">
+                    <label className="flabel" htmlFor="name">Full name</label>
+                    <input className="finput" type="text" id="name" name="name" onChange={handleChange} />
+                    <span className="input-border"></span>
+                </div>
+                <div className="form">
+                    <label className="flabel" htmlFor="email">E-mail</label>
+                    <input className="finput" type="text" id="email" name="email" onChange={handleChange} />
+                    <span className="input-border"></span>
+                </div>
+                <div className="form">
+                    <label className="flabel" htmlFor="phone">Phone number</label>
+                    <input className="finput" type="text" id="phone" name="phone" onChange={handleChange} />
+                    <span className="input-border"></span>
+                </div>
+                <div className="form">
+                    <label className="flabel" htmlFor="passphrase">Password</label>
+                    <input className="finput" type="password" id="passphrase" name="passphrase" onChange={handleChange} />
+                    <span className="input-border"></span>
+                </div>
+                <div className="form">
+                    <label className="flabel" htmlFor="c_passphrase">Confirm password</label>
+                    <input className="finput" type="password" id="c_passphrase" name="c_passphrase" onChange={handleChange} />
+                    <span className="input-border"></span>
+                </div>
+                <br />
+                <button className="form-submit" onClick={validateForm}><span className="text">Register</span><span>Hello</span></button>
+                <p className="error">{errorMessage}</p>
+                <br />
             </div>
         </>
-      );
-    };
+    );
+};
+
 export default Registration;
